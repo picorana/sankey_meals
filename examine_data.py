f96ad586cf721964ef505226eacaad3b61ee9fcf
picorana <dibartolomeo.sara@gmail.com>
2018-09-18 11:24:52 -0400

    first commit

---
+++ examine_data.py
@@ -0,0 +1,152 @@
+from datetime import datetime as dt
+import colorlover as cl
+import json
+from collections import defaultdict
+import csv
+
+meal_dict = defaultdict(int)
+days_dict = {}
+
+meal_index = []
+result_dict = {
+    "nodes" : [],
+    "links" : []
+}
+
+
+def extract_glucose_range(value):
+    if value < 54: return 'very_low'
+    elif value < 70: return 'low'
+    elif value < 180: return 'normal'
+    elif value < 250: return 'high'
+    else: return 'very_high'
+
+
+def get_color(value):
+    if value < 54: return cl.scales['10']['div']['RdYlBu'][8]
+    elif value < 70: return cl.scales['10']['div']['RdYlBu'][6]
+    elif value < 180: return cl.scales['10']['div']['RdYlBu'][4]
+    elif value < 250: return cl.scales['10']['div']['RdYlBu'][3]
+    else: return cl.scales['10']['div']['RdYlBu'][2]
+
+
+
+def is_same_day(date1, date2):
+    if date1.day == date2.day and date1.month == date2.month and date1.year == date2.year: return True
+    else: return False
+
+
+def examine_day(day):
+
+    breakfast_eaten = False
+    lunch_eaten = False
+    dinner_eaten = False
+
+    for i in range(len(day)-1):
+        meal = day[i]
+        next_meal = day[i+1]
+
+        meal['Glucose'] = float(meal['Glucose'])
+        next_meal['Glucose'] = float(next_meal['Glucose'])
+
+        if meal['Meal'] == 'Breakfast': breakfast_eaten = True
+        if meal['Meal'] == 'Lunch': lunch_eaten = True
+        if meal['Meal'] == 'Dinner': dinner_eaten = True
+
+        if meal['Meal'] == 'Afternoon Snack' or meal['Meal'] == 'Bedtime Snack' or meal['Meal'] == 'Exercise Snack' or meal['Meal'] == 'Sugar to treat':
+            meal['Meal'] = 'Snack'
+
+        if meal['Meal'] == 'Snack':
+            if not breakfast_eaten: meal['Meal'] = 'Early_Snack'
+            elif not lunch_eaten: meal['Meal'] = 'AM_Snack'
+            elif not dinner_eaten: meal['Meal'] = 'PM_Snack'
+            else: meal['Meal'] = 'Bedtime_Snack'
+
+        if next_meal['Meal'] == 'Afternoon Snack' or next_meal['Meal'] == 'Bedtime Snack' or next_meal['Meal'] == 'Exercise snack' or next_meal['Meal'] == 'Sugar to treat':
+            next_meal['Meal'] = 'Snack'
+
+        if next_meal['Meal'] == 'Snack':
+            if not breakfast_eaten: next_meal['Meal'] = 'Early_Snack'
+            elif not lunch_eaten: next_meal['Meal'] = 'AM_Snack'
+            elif not dinner_eaten: next_meal['Meal'] = 'PM_Snack'
+            else: next_meal['Meal'] = 'Bedtime_Snack'
+
+        meal_full_name = meal['Meal'] + "_" + extract_glucose_range(meal['Glucose'])
+        next_meal_full_name = next_meal['Meal'] + "_" + extract_glucose_range(next_meal['Glucose'])
+
+        if meal_full_name not in meal_index:
+            meal_index.append(meal_full_name)
+            result_dict['nodes'].append({
+                "name": meal_full_name,
+                "color": get_color(meal['Glucose']),
+                "level" : extract_glucose_range(meal['Glucose']),
+                "type" : meal['Meal']
+            })
+
+        if next_meal_full_name not in meal_index:
+            meal_index.append(next_meal_full_name)
+            result_dict['nodes'].append({
+                "name":next_meal_full_name,
+                "color": get_color(next_meal['Glucose']),
+                "level" : extract_glucose_range(next_meal['Glucose']),
+                "type" : next_meal['Meal']
+            })
+
+
+        if meal['Meal'] != 'Sugar to treat' and meal['Meal'] == next_meal['Meal']: continue
+
+        for link in result_dict["links"]:
+            if link["source"] == meal_index.index(meal_full_name):
+                if link["target"] == meal_index.index(next_meal_full_name):
+                    link["value"] += 1
+                    return
+        result_dict["links"].append({
+            "source" : meal_index.index(meal_full_name),
+            "target" : meal_index.index(next_meal_full_name),
+            "value" : 1
+        })
+
+
+def export_to_json():
+    result_file = open('result.json', 'w+')
+    json.dump(result_dict, result_file, indent=4)
+
+
+def read_events():
+    date = None
+    useful_rows_count = 0
+
+    for row in csv.DictReader(open('data/event.csv', 'r'), delimiter = ','):
+        if len(row['Meal']) < 1: continue # pay attention to this! what are the missing rows, are they just logs?
+        if 'Other' in row['Meal'] or 'Nothing' in row['Meal'] : continue
+        if len(row['Glucose']) < 1: continue # this too!! what is this?
+
+        if row['Meal'] == 'Lunch' and dt.strptime(row['time'], '%Y-%m-%d %H:%M:%S').hour == 23: continue
+
+        # if row['Meal'] == 'Afternoon snack' or row['Meal'] == 'Bedtime snack' or row['Meal'] == 'Exercise snack' or row['Meal'] == 'Snack' or 'Sugar to treat' in row['Meal']:
+            # continue
+            # if dt.strptime(row['time'], '%Y-%m-%d %H:%M:%S').hour < 12:
+                # row['Meal'] = 'AM_Snack'
+            # else: row['Meal'] = 'PM_Snack'
+
+            # row['Meal'] = row['Meal'] + '_' + str(dt.strptime(row['time'], '%Y-%m-%d %H:%M:%S').hour)
+
+        if date == None or not is_same_day(dt.strptime(row['time'], '%Y-%m-%d %H:%M:%S'), date):
+            date = dt.strptime(row['time'], '%Y-%m-%d %H:%M:%S')
+            days_dict[date] = []
+            days_dict[date].append(row)
+        else:
+            days_dict[date].append(row)
+
+        useful_rows_count += 1
+        if useful_rows_count % 220 == 0: break
+
+
+    for day in days_dict:
+        examine_day(days_dict[day])
+
+    print('found ' + str(useful_rows_count) + ' rows with useful data')
+
+
+read_events()
+export_to_json()
---
+++ main.js
@@ -0,0 +1,184 @@
+var svg = d3.select("svg"),
+  width = +svg.attr("width"),
+  height = +svg.attr("height")-100;
+
+var formatNumber = d3.format(",.0f"),
+  format = function(d) {
+    return formatNumber(d) + " times";
+  },
+  color = d3.scaleOrdinal(d3.schemeCategory10);
+
+var sankey = d3
+  .sankey()
+  .nodeWidth(15)
+  .nodePadding(10)
+  .extent([[1, 1], [width - 1, height - 6]]);
+
+var link = svg
+  .append("g")
+  .attr("class", "links")
+  .attr("fill", "none")
+  .attr("stroke", "#000")
+  .attr("stroke-opacity", 0.2)
+  .selectAll("path");
+
+var node = svg
+  .append("g")
+  .attr("class", "nodes")
+  .attr("font-family", "sans-serif")
+  .attr("font-size", 10)
+  .selectAll("g");
+
+var graph;
+
+d3.json("result.json", function(error, energy) {
+  if (error) throw error;
+
+  graph = sankey(energy);
+
+  link = link
+    .data(energy.links)
+    .enter()
+    .append("path")
+    .attr("d", d3.sankeyLinkHorizontal())
+    .attr("stroke-width", function(d) {
+      return Math.max(1, d.width);
+    });
+
+  link.append("title").text(function(d) {
+    return d.source.name + " → " + d.target.name + "\n" + format(d.value);
+  });
+
+  node = node
+    .data(energy.nodes)
+    .enter()
+    .append("g")
+    .call(
+      d3
+        .drag()
+        .subject(function(d) {
+          return d;
+        })
+        .on("start", function() {
+          this.parentNode.appendChild(this);
+        })
+        .on("drag", dragmove)
+    );
+
+  node
+    .append("rect")
+    .attr("x", function(d) {
+      return d.x0;
+    })
+    .attr("y", function(d) {
+      return d.y0;
+    })
+    .attr("height", function(d) {
+      return d.y1 - d.y0;
+    })
+    .attr("width", function(d) {
+      return d.x1 - d.x0;
+    })
+    .attr("fill", function(d) {
+      return d.color;
+    });
+  // .attr("stroke", "#000");
+
+  node
+    .append("text")
+    .attr("x", function(d) {
+      return d.x0 - 6;
+    })
+    .attr("y", function(d) {
+      return (d.y1 + d.y0) / 2;
+    })
+    .attr("dy", "0.35em")
+    .attr("text-anchor", "end")
+    .text(function(d) {
+      return d.name;
+    })
+    .filter(function(d) {
+      return d.x0 < width / 2;
+    })
+    .attr("x", function(d) {
+      return d.x1 + 6;
+    })
+    .attr("text-anchor", "start");
+
+  node.append("title").text(function(d) {
+    return d.name + "\n" + format(d.value);
+  });
+
+  //graph.nodes.forEach(function(node){node.x0 = 0; console.log(node)})
+  relayout();
+});
+
+// the function for moving the nodes
+function dragmove(d) {
+  var rectY = d3
+    .select(this)
+    .select("rect")
+    .attr("y");
+
+  d.y0 = d.y0 + d3.event.dy;
+
+  var yTranslate = d.y0 - rectY;
+
+  d3.select(this).attr("transform", "translate(0" + "," + yTranslate + ")");
+
+  sankey.update(graph);
+  link.attr("d", d3.sankeyLinkHorizontal());
+}
+
+function relayout() {
+
+  d3.selectAll("text").remove();
+
+  columns = ['Early_Snack', 'Breakfast', 'AM_Snack', 'Lunch', 'PM_Snack', 'Dinner', 'Bedtime_Snack']
+  levels = ['very_high', 'high', 'normal', 'low', 'very_low']
+
+  rect_width = 10;
+
+  columns.forEach(function(col, i){
+    svg.append("text")
+      .attr("x", i*width/columns.length)
+      .attr("y", height + 80)
+      .style("font", "10px sans-serif")
+      .text(col)
+  })
+
+  d3.selectAll("rect").each(function(d, i) {
+    cur_rect = this;
+    cur_meal = null;
+    cur_level = null;
+
+    columns.forEach(function(col, i){
+      if (d.type == col) { 
+        d.x0 = i*width/columns.length;
+        d.x1 = i*width/columns.length + rect_width;
+        d3.select(cur_rect).attr("x", i*width/columns.length);
+        cur_meal = col;
+      } 
+    })
+
+    
+    final_height = 0;
+
+    d3.selectAll("rect").each(function(r, j){
+      if (r.type == cur_meal && levels.indexOf(r.level) < levels.indexOf(d.level)) {
+          console.log(d3.select(this).attr('height'));
+          final_height += parseFloat(d3.select(this).attr('height')) + 10;
+        } 
+    })
+
+    d.y0 = final_height;
+    d3.select(cur_rect).attr('y', final_height);
+
+
+
+  });
+
+  sankey.update(graph);
+
+  link.attr("d", d3.sankeyLinkHorizontal());
+}
